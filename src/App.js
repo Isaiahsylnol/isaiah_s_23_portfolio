96048392c97da361744a5d5d97cea43593dda8e7
Isaiahsylnol <pictureisaiah@gmail.com>
2023-03-09 10:07:23 -0500

    Changes to package

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Profile from "./Pages/index";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-import Switcher from "./components/toggleDarkMode";
 
 function App() {
   return (
